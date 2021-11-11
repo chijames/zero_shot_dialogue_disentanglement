0a5ba2d84bd94805696d32ef27cf754582351f66
xxx <xxx@example.com>
2021-11-10 21:22:44 -0500

    update script

--- scratch.sh
+++
@@ -1,9 +0,0 @@
-cd link_based_dialogue_disentanglement/
-
-for i in 12345 1 2
-do
-    for j in 1.0
-    do
-        python main.py --bert_model ~/bert_model_small --output_dir camera_ready_"$i"/scratch --train_dir link_no_time/ --max_num_contexts 50 --test_gold link_no_time/test/*anno* --gpu 0 --use_pretrain --dev_gold link_no_time/dev/*anno* --data_percent $j --seed $i
-    done
-done
