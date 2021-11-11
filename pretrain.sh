d178a79bd66cb4006575d4952c3204185286792d
xxx <xxx@example.com>
2021-11-10 21:12:49 -0500

    update script

--- pretrain.sh
+++ pretrain.sh
@@ -3,5 +3,5 @@ cd entangled_response_selection/
 # test different aux_weight
 for i in 0.25
 do
-    python main.py --bert_model /path/to/bert-model/ --output_dir auxweight_$i/ --train_dir /path/to/dstc8_2 --gpu 0 --eval_batch_size 2 --use_pretrain --max_num_contexts 49 --fp16 --aux_weight $i --train_batch_size 8
+    python main.py --bert_model /path/to/bert-model/ --output_dir auxweight_$i/ --train_dir ../dstc8_2 --gpu 0 --eval_batch_size 2 --use_pretrain --max_num_contexts 49 --fp16 --aux_weight $i --train_batch_size 8
 done
