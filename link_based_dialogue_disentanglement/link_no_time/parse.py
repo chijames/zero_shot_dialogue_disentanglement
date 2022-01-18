79388135fdc19b1a50e0c8afdc08c277c045f91f
xxx <xxx@example.com>
2022-01-18 15:29:01 -0500

    refine parse.py script to make it more easy to understand (logic is exactly the same)

--- link_based_dialogue_disentanglement/link_no_time/parse.py
+++ link_based_dialogue_disentanglement/link_no_time/parse.py
@@ -1,7 +1,5 @@
 import os
-from collections import defaultdict
 import tqdm
-import json
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -10,10 +8,8 @@ parser.add_argument("--num_contexts", type=int)
 parser.add_argument("--mode", type=str)
 args = parser.parse_args()
 
-res = []
-parsed = set()
-
 outfile = open('{}.txt'.format(args.mode), 'w')
+parsed = set()
 
 for filename in tqdm.tqdm(os.listdir(args.dir_path)):
     date = filename.split('.')[0]
@@ -31,59 +27,46 @@ for filename in tqdm.tqdm(os.listdir(args.dir_path)):
     # read text file
     text = open(os.path.join(args.dir_path, date+'.ascii.txt')).read().splitlines()
     
-    max_context = args.num_contexts
-    # we assume a message only relates to at most max_context sentences preceding it
-    # sliding window
-    slide = 1
-    # starting from 1000
-    for i in range(1000, len(text), slide): # 0-based
-        childbyparent = {} # key/child is the later sentence, value/parent is the preceding sentence
-        # however, some pair may have values outside the above range
-        # we simply discard this kind of pairs
-        # inclusive
-        min_id = i
-        max_id = min(i + max_context, len(text)) - 1
-        if max_id - min_id + 1 < max_context:
-            continue
+    # we assume a message only relates to at most args.num_contexts sentences preceding it
+    # the first 1000 sentences (id zero-based) are context without link annotations
+    for min_id in range(1000, len(text)-args.num_contexts+1):
+        max_id = min_id + args.num_contexts
         cur_connections = []
         for connection in connections:
-            if min_id <= connection[0] <= max_id and min_id <= connection[1] <= max_id and connection[0] != connection[1]:
+            # some sentence pairs may have reside outside the [min_id, max_id) range, and we simply discard this kind of pairs
+            # we also discard self link, which will be taken care of later
+            if min_id <= connection[0] < max_id and min_id <= connection[1] < max_id and connection[0] != connection[1]:
                 cur_connections.append(connection)
 
-        for j in range(min_id, max_id+1):
-            tokens = text[j].split()
-            if tokens[0][0] == '[' and tokens[0][-1] == ']': # time, skip
+        # write the text part
+        for i in range(min_id, max_id):
+            tokens = text[i].split()
+            if tokens[0][0] == '[' and tokens[0][-1] == ']': # skip time to be consistent with the entangled response selection training data
                 tokens = tokens[1:]
-                if tokens[0][0] == '<' and tokens[0][-1] == '>': # reduce seq length
+                if tokens[0][0] == '<' and tokens[0][-1] == '>': # a trick to reduce seq length, not important
                     tokens[0] = tokens[0][1:-1]
                 outfile.write('{}\n'.format(' '.join(tokens)))
             else:
-                outfile.write('{}\n'.format(text[j]))
-            
+                outfile.write('{}\n'.format(text[i]))
 
+        child2parent = {} # key/child is the later sentence, value/parent is the preceding sentence that the child points to
         for cc in cur_connections:
-            # we always pick shorter and later links
-            if cc[1] in childbyparent:
-                if cc[0] > childbyparent[cc[1]]:
-                    childbyparent[cc[1]] = cc[0]
-            else:
-                childbyparent[cc[1]] = cc[0]
-        # now, some of the children do not have parents
-        # their parents should be the dummy root
-        # find the intersection
-        id_set = set(list(range(min_id, max_id+1)))
-        diff = id_set - set(list(childbyparent.keys()))
-        for d in diff:
-            childbyparent[d] = float('-inf')
-        # offset, 0 is the dummy root
-        childbyparent = {k-min_id+1: v-min_id+1 for k, v in childbyparent.items()}
-        # add dummy root
-        for k, v in childbyparent.items():
-            if v == float('-inf'):
-                childbyparent[k] = 0
-        # add -1 just for representation
-        # same as turbo parser representation
+            # we always pick the most recent (with a larger id) links
+            child2parent[cc[1]] = max(cc[0], child2parent[cc[1]]) if cc[1] in child2parent else cc[0]
+
+        # some of the children/sentences do not have parents, and their parents should be the dummy root
+        orphans = set(list(range(min_id, max_id))) - set(list(child2parent.keys()))
+        # we make the ids 0-indexed first (-min_id) and then shift all ids by 1 (+1) to accommodate the dummy root (id=0)
+        child2parent = {k-min_id+1: v-min_id+1 for k, v in child2parent.items()}
+        for orphan in orphans:
+            # apply the same 0-indexed and shift by 1 logic
+            child2parent[orphan-min_id+1] = 0
+
+        assert len(child2parent) == args.num_contexts
+        # add -1 just for the consistency with turbo parser
+        # only the last numbers are used in this work
+        # you can safely ignore all the other numbers
         links = [str(-1)]
-        for j in range(1, len(childbyparent)+1):
-            links.append(str(childbyparent[j]))
+        for j in range(1, args.num_contexts+1):
+            links.append(str(child2parent[j]))
         outfile.write('{}\n'.format(' '.join(links)))
