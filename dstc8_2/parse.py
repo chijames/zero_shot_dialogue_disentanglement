ae7af8ab1ae670539ff3a11ce998a1d38500a521
xxx <xxx@example.com>
2022-01-18 16:29:00 -0500

    add comments to make the code easier to understand

--- dstc8_2/parse.py
+++ dstc8_2/parse.py
@@ -1,7 +1,7 @@
 import json
-from collections import defaultdict
 import argparse
 import random
+random.seed(0) # for reproducibility
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--mode', type=str)
@@ -16,36 +16,36 @@ for content in data:
     options = content['options-for-correct-answers']
     if len(options) == 0: # we skip no answers
         continue
+    # handle a formatting bug in the data
     try:
         option = options[0]
     except:
         option = options
-    correct_id = option['candidate-id']
-    correct_text = option['speaker'] + ' ' + option['utterance'].strip()
-    context = []
-    for message in messages:
-        text = message['speaker'] + ' ' + message['utterance']
-        context.append(text.replace('\t', ' '))
+
+    correct_id, correct_text = option['candidate-id'], '{} {}'.format(option['speaker'], option['utterance']).replace('\t', ' ')
+    context = ['{} {}'.format(message['speaker'], message['utterance']).replace('\t', ' ') for message in messages]
     outfile.write('{}\t{}\t{}\n'.format(1, '\t'.join(context), correct_text))
+
     # write negative samples
-    if args.mode != 'train':
-        negs = content['options-for-next']
+    negs = content['options-for-next']
+    if args.mode != 'train': # for evaluation, pos:neg = 1:99
         cnt = 0
         for neg in negs:
             if neg['candidate-id'] != correct_id:
                 cnt += 1
-                neg_text = option['speaker'] + ' ' + neg['utterance'].strip()
+                neg_text = option['speaker'] + ' ' + neg['utterance']
                 outfile.write('{}\t{}\t{}\n'.format(0, '\t'.join(context), neg_text))
+        # handle a bug in the data where some instances do not have 99 negatives...
         if cnt != 99:
             diff = 99 - cnt
             for _ in range(diff):
                 outfile.write('{}\t{}\t{}\n'.format(0, '\t'.join(context), neg_text))
-    if args.mode == 'train':
-        negs = content['options-for-next']
+    else: # for training, pos:neg = 1:1
         neg_indices = list(range(len(negs)))
         while True:
             neg_idx = random.choice(neg_indices)
+            # handle a bug in the data where some instances have the correct id in negatives...
             if negs[neg_idx]['candidate-id'] != correct_id:
-                neg_text = option['speaker'] + ' ' + negs[neg_idx]['utterance'].strip()
+                neg_text = '{} {}'.format(option['speaker'], negs[neg_idx]['utterance'])
                 outfile.write('{}\t{}\t{}\n'.format(0, '\t'.join(context), neg_text))
                 break
