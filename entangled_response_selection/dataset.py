acbf381e5b646d797fe6289a986a57736eb96486
xxx <xxx@example.com>
2022-01-18 16:29:50 -0500

    refactor the code

--- entangled_response_selection/dataset.py
+++ entangled_response_selection/dataset.py
@@ -1,9 +1,5 @@
 import torch
 from torch.utils.data import Dataset
-from tqdm import tqdm
-import os
-import random
-import pickle
 
 
 class SelectionDataset(Dataset):
@@ -17,7 +13,7 @@ class SelectionDataset(Dataset):
                 'labels': []
             }
             for line in f:
-                split = line.strip().split('\t')
+                split = line.strip('\n').split('\t')
                 lbl, context, response = int(split[0]), split[1:-1], split[-1]
                 if lbl == 1 and len(group['responses']) > 0:
                     self.data_source.append(group)
@@ -41,25 +37,21 @@ class SelectionDataset(Dataset):
         group = self.data_source[index]
         context, responses, labels = group['context'], group['responses'], group['labels']
         transformed_text = self.concat_transform(context, responses)
-        ret = transformed_text, labels
 
-        return ret
+        return transformed_text, labels
 
     def batchify_join_str(self, batch):
-        text_token_ids_list_batch, text_input_masks_list_batch, text_segment_ids_list_batch = [], [], []
-        labels_batch = []
+        text_token_ids_list_batch, text_input_masks_list_batch, text_segment_ids_list_batch, labels_batch = [], [], [], []
         for sample in batch:
             text_token_ids_list, text_input_masks_list, text_segment_ids_list = sample[0]
-
             text_token_ids_list_batch.append(text_token_ids_list)
             text_input_masks_list_batch.append(text_input_masks_list)
             text_segment_ids_list_batch.append(text_segment_ids_list)
             labels_batch.append(sample[1])
 
-        long_tensors = [text_token_ids_list_batch, text_input_masks_list_batch, text_segment_ids_list_batch]
+        long_tensors = [text_token_ids_list_batch, text_input_masks_list_batch, text_segment_ids_list_batch, labels_batch]
 
-        text_token_ids_list_batch, text_input_masks_list_batch, text_segment_ids_list_batch = (
+        text_token_ids_list_batch, text_input_masks_list_batch, text_segment_ids_list_batch, labels_batch = (
             torch.tensor(t, dtype=torch.long) for t in long_tensors)
 
-        labels_batch = torch.tensor(labels_batch, dtype=torch.long)
         return text_token_ids_list_batch, text_input_masks_list_batch, text_segment_ids_list_batch, labels_batch
